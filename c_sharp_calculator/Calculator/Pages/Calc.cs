9382ad285e1bddae6e6f7f6456b5e8ff1a65eb52
yarhamjohn <yarhamjohn@gmail.com>
2020-06-12 17:02:07 +0100

    start implementing operators

--- c_sharp_calculator/Calculator/Pages/Calc.cs
+++ c_sharp_calculator/Calculator/Pages/Calc.cs
@@ -1,15 +1,92 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 public static class Calc
 {
     public static decimal Calculate(Calculation calculation)
     {
-        var calc = calculation.GetCalculation();
-        if (calc == "")
+        System.Console.WriteLine(string.Join("", calculation.Current));
+        if (calculation.Current.Count == 0)
         {
             return 0;
         }
 
-        return Convert.ToDecimal(calc);
+        if (IsADecimal(calculation.Current))
+        {
+            return Convert.ToDecimal(string.Join("", calculation.Current));
+        }
+
+        List<string> chunks = GetCalculationChunks(calculation);
+
+        decimal runningTotal = 0;
+        string activeOperator = null;
+        for (int i = 0; i < chunks.Count; i++)
+        {
+            var isDecimal = Decimal.TryParse(chunks[i], out var chunk);
+
+            if (isDecimal && activeOperator == null)
+            {
+                runningTotal = chunk;
+            }
+            else if (isDecimal && activeOperator != null)
+            {
+                switch (activeOperator)
+                {
+                    case "+":
+                        runningTotal += chunk;
+                        break;
+                    case "-":
+                        runningTotal -= chunk;
+                        break;
+                    case "x":
+                        runningTotal *= chunk;
+                        break;
+                    case "÷":
+                        runningTotal /= chunk;
+                        break;
+                }
+            }
+            else
+            {
+                activeOperator = chunks[i];
+            }
+        }
+
+        return runningTotal;
+    }
+
+    private static List<string> GetCalculationChunks(Calculation calculation)
+    {
+        StringBuilder activeChunk = new StringBuilder();
+        List<string> chunks = new List<string>();
+
+        for (int i = 0; i < calculation.Current.Count; i++)
+        {
+            if (IsADecimal(new List<string> { calculation.Current[i] }))
+            {
+                activeChunk.Append(calculation.Current[i]);
+            }
+            else
+            {
+                chunks.Add(activeChunk.ToString());
+                activeChunk.Clear();
+                chunks.Add(calculation.Current[i]);
+            }
+        }
+
+        return chunks;
+    }
+
+    private static bool IsADecimal(List<string> current)
+    {
+        System.Console.WriteLine(current);
+        System.Console.WriteLine(string.Join("", current.Distinct()));
+        System.Console.WriteLine(string.Join("", current.Distinct().Except(new List<string> { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "-" })));
+        return current
+        .Distinct()
+        .Except(new List<string> { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "-" })
+        .Count() == 0;
     }
 }
--- c_sharp_calculator/Calculator/Pages/Calculation.cs
+++ c_sharp_calculator/Calculator/Pages/Calculation.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 public class Calculation
 {
     public List<string> Current;
+    private List<string> operators = new List<string> { "+", "-", "x", "÷" };
+
     public Calculation()
     {
         Current = new List<string>();
     }
 
-    public void AddToCalculation(Button button)
+    public void UpdateCalculation(Button button)
     {
         if (button.DisplayText == "." && Current.Contains("."))
         {
@@ -29,6 +31,24 @@ public class Calculation
             return;
         }
 
+        if (operators.Contains(button.DisplayText))
+        {
+            if (Current.Count == 0)
+            {
+                Current.AddRange(new List<string> { "0", button.DisplayText });
+            }
+            else if (operators.Contains(Current[Current.Count - 1]))
+            {
+                ReplaceLastOperator(button);
+            }
+        }
+
+        Current.Add(button.DisplayText);
+    }
+
+    private void ReplaceLastOperator(Button button)
+    {
+        Current.RemoveAt(Current.Count - 1);
         Current.Add(button.DisplayText);
     }
 
