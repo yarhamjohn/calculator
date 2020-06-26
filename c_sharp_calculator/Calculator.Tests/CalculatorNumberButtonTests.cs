71aebd3e1261f032a4fbf0a34e89b7b7951c14cd
yarhamjohn <yarhamjohn@gmail.com>
2020-06-26 14:19:53 +0100

    simplify

--- c_sharp_calculator/Calculator.Tests/CalculatorNumberButtonTests.cs
+++ c_sharp_calculator/Calculator.Tests/CalculatorNumberButtonTests.cs
@@ -6,53 +6,19 @@ namespace Calculator.Tests
 {
     public class CalculatorNumberButtonClickTests : TestContext
     {
-        [Fact]
-        public void ClickingNumberButton_ReplacesResult_IfResultIsZero()
+        [Theory]
+        [InlineData(new[] { "1" }, "1", "")]
+        [InlineData(new[] { "1", "2" }, "12", "")]
+        [InlineData(new[] { "1", "+/-", "0" }, "-10", "")]
+        [InlineData(new[] { "1", ".", "2" }, "1.2", "")]
+        [InlineData(new[] { "1", "=", "2" }, "2", "1 =")]
+        public void ClickingNumberButton_CorrectlySetsCalculationAndResult(string[] setupButtons, string expectedResult, string expectedCalculation)
         {
             var cut = RenderComponent<CalculatorPage>();
+            foreach (var button in setupButtons) { cut.Find($"button[id='{button}']").Click(); }
 
-            cut.Find($"button[id=1]").Click();
-
-            Assert.Equal("", Utils.GetCalculation(cut));
-            Assert.Equal("1", Utils.GetResult(cut));
-        }
-
-        [Fact]
-        public void ClickingNumberButton_AppendsToResult_IfResultIsAPositiveNumber()
-        {
-            var cut = RenderComponent<CalculatorPage>();
-            cut.Find("button[id=1]").Click();
-
-            cut.Find($"button[id=2]").Click();
-
-            Assert.Equal("", Utils.GetCalculation(cut));
-            Assert.Equal("12", Utils.GetResult(cut));
-        }
-
-        [Fact]
-        public void ClickingNumberButton_AppendsToResult_IfResultIsANegativeNumber()
-        {
-            var cut = RenderComponent<CalculatorPage>();
-            cut.Find("button[id=1]").Click();
-            cut.Find("button[id='+/-']").Click();
-
-            cut.Find($"button[id=0]").Click();
-
-            Assert.Equal("", Utils.GetCalculation(cut));
-            Assert.Equal("-10", Utils.GetResult(cut));
-        }
-
-        [Fact]
-        public void ClickingNumberButton_AppendsResult_IfResultEndsWithDecimal()
-        {
-            var cut = RenderComponent<CalculatorPage>();
-            cut.Find($"button[id=1]").Click();
-            cut.Find($"button[id='.']").Click();
-
-            cut.Find($"button[id=2]").Click();
-
-            Assert.Equal("", Utils.GetCalculation(cut));
-            Assert.Equal("1.2", Utils.GetResult(cut));
+            Assert.Equal(expectedCalculation, Utils.GetCalculation(cut));
+            Assert.Equal(expectedResult, Utils.GetResult(cut));
         }
     }
 }
--- c_sharp_calculator/Calculator.Tests/CalculatorSignButtonTests.cs
+++ c_sharp_calculator/Calculator.Tests/CalculatorSignButtonTests.cs
@@ -6,45 +6,21 @@ namespace Calculator.Tests
 {
     public class SignButtonClickTests : TestContext
     {
-        [Fact]
-        public void ClickingSignButton_DoesNotChangeResult_IfResultIsZero()
-        {
-            var cut = RenderComponent<CalculatorPage>();
-
-            cut.Find($"button[id='+/-']").Click();
-
-            Assert.Equal("", Utils.GetCalculation(cut));
-            Assert.Equal("0", Utils.GetResult(cut));
-        }
-
         [Theory]
-        [InlineData(new[] { "1" }, "-1")]
-        [InlineData(new[] { "1", "." }, "-1.")]
-        [InlineData(new[] { "1", ".", "2" }, "-1.2")]
-        public void ClickingSignButton_ConvertsPositiveResultToNegative(string[] setupButtons, string expected)
+        [InlineData(new[] { "+/-" }, "0", "")]
+        [InlineData(new[] { "1", "+/-" }, "-1", "")]
+        [InlineData(new[] { "1", ".", "+/-" }, "-1.", "")]
+        [InlineData(new[] { "1", ".", "2", "+/-" }, "-1.2", "")]
+        [InlineData(new[] { "1", "+/-", "+/-" }, "1", "")]
+        [InlineData(new[] { "1", "+/-", ".", "+/-" }, "1.", "")]
+        [InlineData(new[] { "1", "+/-", ".", "2", "+/-" }, "1.2", "")]
+        public void ClickingSignButton_CorrectlySetsCalculationAndResult(string[] setupButtons, string expectedResult, string expectedCalculation)
         {
             var cut = RenderComponent<CalculatorPage>();
             foreach (var button in setupButtons) { cut.Find($"button[id='{button}']").Click(); }
 
-            cut.Find($"button[id='+/-']").Click();
-
-            Assert.Equal("", Utils.GetCalculation(cut));
-            Assert.Equal(expected, Utils.GetResult(cut));
-        }
-
-        [Theory]
-        [InlineData(new[] { "1", "+/-" }, "1")]
-        [InlineData(new[] { "1", "+/-", "." }, "1.")]
-        [InlineData(new[] { "1", "+/-", ".", "2" }, "1.2")]
-        public void ClickingSignButton_ConvertsNegativeResultToPositive(string[] setupButtons, string expected)
-        {
-            var cut = RenderComponent<CalculatorPage>();
-            foreach (var button in setupButtons) { cut.Find($"button[id='{button}']").Click(); }
-
-            cut.Find($"button[id='+/-']").Click();
-
-            Assert.Equal("", Utils.GetCalculation(cut));
-            Assert.Equal(expected, Utils.GetResult(cut));
+            Assert.Equal(expectedCalculation, Utils.GetCalculation(cut));
+            Assert.Equal(expectedResult, Utils.GetResult(cut));
         }
     }
 }
