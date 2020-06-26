82cc15c3e020da34412c39092326d03a2613e491
yarhamjohn <yarhamjohn@gmail.com>
2020-06-26 16:05:37 +0100

    add clear buttons

---
+++ c_sharp_calculator/Calculator.Tests/CalculatorClearButtonTests.cs
@@ -0,0 +1,78 @@
+using Xunit;
+using Bunit;
+using Calculator.Pages;
+
+namespace Calculator.Tests
+{
+    public class ClearButtonClickTests : TestContext
+    {
+        [Theory]
+        [InlineData(new[] { "C" }, "0", "")]
+        [InlineData(new[] { "1", "C" }, "0", "")]
+        [InlineData(new[] { ".", "C" }, "0", "")]
+        [InlineData(new[] { "=", "C" }, "0", "")]
+        [InlineData(new[] { "+/-", "C" }, "0", "")]
+        [InlineData(new[] { "1", "2", "C" }, "0", "")]
+        [InlineData(new[] { "1", "+/-", "C" }, "0", "")]
+        [InlineData(new[] { "1", ".", "C" }, "0", "")]
+        [InlineData(new[] { "1", ".", "2", "C" }, "0", "")]
+        [InlineData(new[] { "1", ".", "2", "+/-", "C" }, "0", "")]
+        [InlineData(new[] { "1", "=", "C" }, "0", "")]
+        [InlineData(new[] { "1", "=", "2", "C" }, "0", "")]
+        [InlineData(new[] { "1", "=", "2", "=", "C" }, "0", "")]
+        public void ClickingClearButton_CorrectlySetsCalculationAndResult(string[] setupButtons, string expectedResult, string expectedCalculation)
+        {
+            var cut = RenderComponent<CalculatorPage>();
+            foreach (var button in setupButtons) { cut.Find($"button[id='{button}']").Click(); }
+
+            Assert.Equal(expectedCalculation, Utils.GetCalculation(cut));
+            Assert.Equal(expectedResult, Utils.GetResult(cut));
+        }
+
+        [Theory]
+        [InlineData(new[] { "CE" }, "0", "")]
+        [InlineData(new[] { "1", "CE" }, "0", "")]
+        [InlineData(new[] { ".", "CE" }, "0", "")]
+        [InlineData(new[] { "=", "CE" }, "0", "0 =")]
+        [InlineData(new[] { "+/-", "CE" }, "0", "")]
+        [InlineData(new[] { "1", "2", "CE" }, "0", "")]
+        [InlineData(new[] { "1", "+/-", "CE" }, "0", "")]
+        [InlineData(new[] { "1", ".", "CE" }, "0", "")]
+        [InlineData(new[] { "1", ".", "2", "CE" }, "0", "")]
+        [InlineData(new[] { "1", ".", "2", "+/-", "CE" }, "0", "")]
+        [InlineData(new[] { "1", "=", "CE" }, "0", "1 =")]
+        [InlineData(new[] { "1", "=", "2", "CE" }, "0", "1 =")]
+        [InlineData(new[] { "1", "=", "2", "=", "CE" }, "0", "2 =")]
+        public void ClickingClearEntryButton_CorrectlySetsCalculationAndResult(string[] setupButtons, string expectedResult, string expectedCalculation)
+        {
+            var cut = RenderComponent<CalculatorPage>();
+            foreach (var button in setupButtons) { cut.Find($"button[id='{button}']").Click(); }
+
+            Assert.Equal(expectedCalculation, Utils.GetCalculation(cut));
+            Assert.Equal(expectedResult, Utils.GetResult(cut));
+        }
+
+        [Theory]
+        [InlineData(new[] { "<-" }, "0", "")]
+        [InlineData(new[] { "1", "<-" }, "0", "")]
+        [InlineData(new[] { ".", "<-" }, "0", "")]
+        [InlineData(new[] { "=", "<-" }, "0", "0 =")]
+        [InlineData(new[] { "+/-", "<-" }, "0", "")]
+        [InlineData(new[] { "1", "2", "<-" }, "1", "")]
+        [InlineData(new[] { "1", "+/-", "<-" }, "0", "")]
+        [InlineData(new[] { "1", ".", "<-" }, "1", "")]
+        [InlineData(new[] { "1", ".", "2", "<-" }, "1.", "")]
+        [InlineData(new[] { "1", ".", "2", "+/-", "<-" }, "-1.", "")]
+        [InlineData(new[] { "1", "=", "<-" }, "0", "1 =")]
+        [InlineData(new[] { "1", "=", "2", "<-" }, "0", "1 =")]
+        [InlineData(new[] { "1", "=", "2", "=", "<-" }, "0", "2 =")]
+        public void ClickingDeleteButton_CorrectlySetsCalculationAndResult(string[] setupButtons, string expectedResult, string expectedCalculation)
+        {
+            var cut = RenderComponent<CalculatorPage>();
+            foreach (var button in setupButtons) { cut.Find($"button[id='{button}']").Click(); }
+
+            Assert.Equal(expectedCalculation, Utils.GetCalculation(cut));
+            Assert.Equal(expectedResult, Utils.GetResult(cut));
+        }
+    }
+}
