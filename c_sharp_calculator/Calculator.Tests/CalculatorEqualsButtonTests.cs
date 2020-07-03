5938957563ff7cf807357a389729d8c9bb9aeddc
yarhamjohn <yarhamjohn@gmail.com>
2020-07-03 16:54:38 +0100

    fix more tests

--- c_sharp_calculator/Calculator.Tests/CalculatorEqualsButtonTests.cs
+++ c_sharp_calculator/Calculator.Tests/CalculatorEqualsButtonTests.cs
@@ -22,6 +22,8 @@ namespace Calculator.Tests
         [InlineData(new[] { "1", "+", "2", "=", "=" }, "5", "3 + 2 =")]
         [InlineData(new[] { "1", "+", "2", "-", "2", "=", "=" }, "-1", "1 - 2 =")]
         [InlineData(new[] { "1", "+", "CE", "=" }, "1", "1 + 0 =")]
+        [InlineData(new[] { "1", "+", "2", "x", "3", "=" }, "6", "1 + 2 x 3 =")]
+        [InlineData(new[] { "1", "-", "1", "+/-", "=" }, "2", "1 - -1 =")]
         public void ClickingEqualsButton_CorrectlySetsCalculationAndResult(string[] setupButtons, string expectedResult, string expectedCalculation)
         {
             var cut = RenderComponent<CalculatorPage>();
